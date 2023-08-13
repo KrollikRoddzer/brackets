8593b5c1511842fe5f930b8a67e751265f4eaa4d
KrollikRoddzer <krollikroddzer@mail.ru>
2023-08-13 19:39:36 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrOpen = [];
+  let same = [];
+  bracketsConfig.forEach(item =>
+      {
+        if (item[0] == item[1])
+        {
+          same.push(item[0]);
+        }
+        else
+        {
+          arrOpen.push(item[0]);
+        }
+      }
+  );
+  let stack = [];
+  for (let i = 0; i < str.length; ++i)
+  {
+    if (same.includes(str[i]))
+    {
+      if (stack.length != 0 && stack[stack.length - 1] == str[i])
+      {
+        stack.pop();
+      }
+      else
+      {
+        stack.push(str[i]);
+      }
+    }
+    else if (arrOpen.includes(str[i]))
+    {
+      stack.push(str[i]);
+    }
+    else
+    {
+      if (stack.length != 0 && stack[stack.length - 1] == findOpposite(bracketsConfig, str[i]))
+      {
+        stack.pop();
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+
+  return stack.length == 0;
+}
+
+function findOpposite(config, item)
+{
+  for (let i = 0; i < config.length; ++i)
+  {
+    if (config[i][1] == item) return config[i][0];
+  }
 }
